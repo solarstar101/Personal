cf40acabb630f3c04a4c92cbae4ff110734b5c52
solarstar101 <45614244+solarstar101@users.noreply.github.com>
2019-06-12 11:46:18 -0500

    Color Update

--- js/myscript.js
+++ js/myscript.js
@@ -6,8 +6,8 @@ window.jQuery = window.$ = jQuery;
 /*-----------------------------------------------------------------------------------*/
 jQuery(window).load(function () {		
 	//Preloader
-	setTimeout("jQuery('#preloader').animate({'opacity' : '0'},300,function(){jQuery('#preloader').hide()})",800);
-	setTimeout("jQuery('.preloader_hide, .selector_open').animate({'opacity' : '1'},500)",800);
+	setTimeout("jQuery('#preloader').animate({'opacity' : '0'},300,function(){jQuery('#preloader').hide()})",2500);
+	setTimeout("jQuery('.preloader_hide, .selector_open').animate({'opacity' : '1'},500)",2500);
 
 });
 
